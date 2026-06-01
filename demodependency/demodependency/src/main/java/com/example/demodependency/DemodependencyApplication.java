74275f145a23e23034dd526171bf65531495ed85
secnt243 <v.manoj2194@gmail.com>
2026-06-01 22:25:28 +0530

    asdsd

---
+++ demodependency/demodependency/src/main/java/com/example/demodependency/DemodependencyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demodependency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodependencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodependencyApplication.class, args);
+	}
+
+}
---
+++ demodependency/demodependency/src/test/java/com/example/demodependency/DemodependencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demodependency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemodependencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
